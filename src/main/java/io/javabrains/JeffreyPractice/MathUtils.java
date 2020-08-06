f36883c7a297a01fee8c13cdb680cfab67085a50
Jeffrey McDonald <jffrmcdonald@gmail.com>
2020-08-06 11:26:32 -0700

    added the divide method and test

--- src/main/java/io/javabrains/JeffreyPractice/MathUtils.java
+++ src/main/java/io/javabrains/JeffreyPractice/MathUtils.java
@@ -7,9 +7,26 @@ public class MathUtils {
 		return a+b;
 	}
 	
+	public int subtract(int a, int b)
+	{
+		return a-b;
+	}
+	
+	public int multiply(int a, int b)
+	{
+		return a*b;
+	}
+	
+	public int divide(int a, int b)
+	{
+		return a/b;
+	}
+	
 	public double computeCircleRadius(double radius)
 	{
 		return radius*radius*Math.PI;
 	}
+	
+	
 
 }
--- src/test/java/io/javabrains/JeffreyPractice/MathUtilsTest.java
+++ src/test/java/io/javabrains/JeffreyPractice/MathUtilsTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class MathUtilsTest {
 
 	@Test
-	void test() {
+	void testAdd() {
 		MathUtils mathUtils = new MathUtils();
 		
 		int expected = 2;
@@ -16,12 +16,20 @@ class MathUtilsTest {
 		assertEquals(expected,actual, "The add method should add two numbers");
 	}
 	
+
+	@Test
+	void testDivide()
+	{
+		MathUtils mathUtils = new MathUtils();
+		assertThrows(ArithmeticException.class,()-> mathUtils.divide(1, 0), "divides two numbers, divide by zero number" );
+	}
+	
 	@Test
 	void testComputeCircleRadius()
 	{
 		MathUtils mathUtils = new MathUtils();
 		assertEquals(314.1592653589793,mathUtils.computeCircleRadius(10),"Should return the radius of the circle");
-		
 	}
+	
 
 }
