4c876a30908cdae79f88a521a266e01135e3bac2
Jeffrey McDonald <jffrmcdonald@gmail.com>
2020-08-06 12:31:37 -0700

    added the rest of the methods

--- src/test/java/io/javabrains/JeffreyPractice/MathUtilsTest.java
+++ src/test/java/io/javabrains/JeffreyPractice/MathUtilsTest.java
@@ -1,11 +1,19 @@
 package io.javabrains.JeffreyPractice;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
+	MathUtils mathUtils;
+	
+	@BeforeEach
+	void init()
+	{
+		mathUtils = new MathUtils();
+	}
+	
 	@Test
 	void testAdd() {
 		MathUtils mathUtils = new MathUtils();
@@ -16,6 +24,27 @@ class MathUtilsTest {
 		assertEquals(expected,actual, "The add method should add two numbers");
 	}
 	
+	@Test
+	void testSubtract()
+	{
+		int expected = 2;
+		int actual = mathUtils.subtract(4,2);
+		
+		assertEquals(expected,actual, "The subtract method should subtract two numbers ");
+		
+		
+	}
+	
+	
+	@Test
+	void testMultiply()
+	{
+		int expected = 20;
+		int actual = mathUtils.multiply(4, 5);
+		
+		assertEquals(expected,actual, "The multiply method should multiply two numbers ");
+		
+	}
 
 	@Test
 	void testDivide()
